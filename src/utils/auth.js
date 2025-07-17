1f21d8dd45006b9cde5fc36f6a886cdde159a005
2godong <sora050821@gmail.com>
2025-07-17 13:58:30 +0900

    2025 배포 세션

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,19 @@
 export const isLoggedIn = () => {
-  return localStorage.getItem('isLoggedIn') === 'true';
+  return localStorage.getItem("isLoggedIn") === "true";
 };
 
+// env파일에 있는 변수 꺼내오기
 export const login = (id, password) => {
-  if (id === `${import.meta.env.VITE_ID}` && password === `${import.meta.env.VITE_PW}`) {
-    localStorage.setItem('isLoggedIn', 'true');
+  if (
+    id === `${import.meta.env.VITE_ID}` &&
+    password === `${import.meta.env.VITE_PW}`
+  ) {
+    localStorage.setItem("isLoggedIn", "true");
     return true;
   }
   return false;
 };
 
 export const logout = () => {
-  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem("isLoggedIn");
 };
